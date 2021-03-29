7468c124a893b7cdc6adb04dc472837e5d33891f
Ravi Kumar <ravikumar.kurva@7-11.com>
2021-03-29 00:31:02 -0500

    March 29, 2021 12:31 AM

--- InterviewBootcamp/BigO/index.js
+++ InterviewBootcamp/BigO/index.js
@@ -1,15 +1,15 @@
 const input = new Array(100000).fill("ravi");
 
 function simpleSearch(array) {
-  const t1 = process.hrtime();
+  const hrStart = process.hrtime();
   for (let i = 0; i < array.length; i++) {
     const element = array[i];
     if (element === "kumar") {
       console.log("fount");
     }
   }
-  const t2 = process.hrtime(t1);
-  console.log(t2, "nanoseconds");
+  const hrEnd = process.hrtime(hrStart);
+  console.log(hrEnd, "nanoseconds");
 }
 
 simpleSearch(input);
